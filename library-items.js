7864e3a7bf6aa5712ad3397e302e5fa176d62552
Carnet-de-Pitou <drelnas.khan@gmail.com>
2026-08-22 21:22:47 +0200

    Met à jour l’index des textes

--- library-items.js
+++ library-items.js
@@ -1,1 +1,1 @@
-window.PITOU_LIBRARY_ITEM_SLUGS = ["tranches-de-vies-et-tranches-de-lard","un-pere-et-sa-fille"];
+window.PITOU_LIBRARY_ITEM_SLUGS = ["tranches-de-vies-et-tranches-de-lard","un-pere-et-sa-fille","bile-cornes-acier"];
