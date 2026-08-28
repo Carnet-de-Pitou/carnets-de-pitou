358c25835ced83137dfc6d5320c4bc6b082cda9d
Carnet-de-Pitou <drelnas.khan@gmail.com>
2026-08-28 22:26:58 +0200

    Ajoute l'ambiance Age of Conan par rubrique

--- audio.js
+++ audio.js
@@ -5,7 +5,8 @@
 
   const DEFAULT_TRACK = 'assets/ambiance-carnets-de-pitou-v2-1.mp3';
   const CATEGORY_TRACKS = {
-    'Les Sentiers du Salut ~~ Rift': 'assets/rift-drelnas-1.mp3'
+    'Les Sentiers du Salut ~~ Rift': 'assets/rift-drelnas-1.mp3',
+    'Les Sentiers de la Solitude ~~ Age of Conan': 'assets/age-of-conan-drelnas.mp3'
   };
 
   audio.volume = 0.38;
